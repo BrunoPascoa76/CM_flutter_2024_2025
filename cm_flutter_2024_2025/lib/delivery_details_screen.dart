b334049f2d2e31a05c5873e4042000090caae19d
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-10-19 14:36:25 +0100

    temp commit to merge

--- cm_flutter_2024_2025/lib/delivery_details_screen.dart
+++ cm_flutter_2024_2025/lib/delivery_details_screen.dart
@@ -11,12 +11,20 @@ class DeliveryDetailsScreen extends StatelessWidget{
     Delivery? currentDelivery=context.watch<DeliveryRouteBloc>().state?.getCurrentDelivery();
     return Scaffold(
       appBar: AppBar(title: const Text('Delivery')),
-      body: Column(children:currentDelivery==null?
-        [
-          Text("All packages have been delivered")
-        ]:[
-          Text("abcdef")
-        ])
+      body: Padding(
+        padding: const EdgeInsets.only(top:8.0),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:currentDelivery==null?
+              const [
+                Text("All packages have been delivered!",style:TextStyle(fontSize: 20))
+              ]:const [
+                Text("abcdef")
+              ]),
+        ),
+      )
     );
   }
 }
