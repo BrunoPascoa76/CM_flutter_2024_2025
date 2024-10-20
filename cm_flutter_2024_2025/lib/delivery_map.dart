4dfedaf2fd7f7dfb0146725daf41b7b82d10132e
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-10-20 16:09:39 +0100

    added qr scan button on map

--- cm_flutter_2024_2025/lib/delivery_map.dart
+++ cm_flutter_2024_2025/lib/delivery_map.dart
@@ -1,4 +1,5 @@
 import 'package:cm_flutter_2024_2025/map.dart';
+import 'package:cm_flutter_2024_2025/utils/qr_code_scanner.dart';
 import 'package:cm_flutter_2024_2025/zoom_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -21,13 +22,20 @@ class DeliveryMapScreen extends StatelessWidget {
             tooltip: 'Zoom In',
             child: const Icon(Icons.add),
           ),
-          const SizedBox(height: 8),
+          const SizedBox(height: 5),
           FloatingActionButton(
             heroTag: 'zoomOut',
             onPressed: () => context.read<ZoomCubit>().zoomOut(),
             tooltip: 'Zoom Out',
             child: const Icon(Icons.remove),
           ),
+          const SizedBox(height: 20),
+          FloatingActionButton(
+            heroTag: "qrCode",
+            onPressed: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>QrCodeScanner())),
+            tooltip: 'Scan QR Code',
+            child: const Icon(Icons.qr_code)
+          )
         ],
       ),
     );
